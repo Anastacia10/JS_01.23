59aff2eac94f1cc95b9210c58617704396af403f
Anastacia10 <Anastacia10Job@gmail.com>
2023-02-09 21:57:37 +0100

    [4.2:09.02]: finished seconf task

--- index.js
+++ index.js
@@ -27,3 +27,42 @@ const curry = (func) => {
     }
   };
 };
+class Calculator {
+  constructor(x, y) {
+    if (isAllNumbers([x, y])) {
+      this.x = x;
+      this.y = y;
+    } else {
+      throw new Error();
+    }
+  }
+  setX = (x) => {
+    if (!isNumber(x)) {
+      throw new Error();
+    } else {
+      this.x = x;
+    }
+  };
+  setY = (y) => {
+    if (!isNumber(y)) {
+      throw new Error();
+    } else {
+      this.y = y;
+    }
+  };
+  getSum = () => {
+    return this.x + this.y;
+  };
+  getSub = () => {
+    return this.x - this.y;
+  };
+  getMul = () => {
+    return this.x * this.y;
+  };
+  getDiv = () => {
+    if (this.y === 0) {
+      throw new Error();
+    }
+    return this.x / this.y;
+  };
+}
