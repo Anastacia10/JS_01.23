94bd29589a5cbb0944c2563b6d21b653b0be5838
Anastacia10 <Anastacia10Job@gmail.com>
2023-02-09 14:00:35 +0100

    [3.2:09.02]:added validation parametres

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const isValidThis = (value) => {
+  return (
+    Object.prototype.toString.call(value) == "[object Array]" ||
+    typeof thisArg === "undefined"
+  );
+};
+
+const isValidTime = (value) => {
+  return value >= 0 && Number.isInteger(value) && Number.isSafeInteger(value);
+};
+
 Array.prototype.customFilter = function (cb, thisArg) {
-  if (typeof cb !== "function") {
-    throw new Error("Callback is not a function");
+  if (typeof cb !== "function" || !isValidThis(thisArg)) {
+    throw new Error();
   }
   let context = this;
   if (thisArg && Object.prototype.toString.call(thisArg) == "[object Array]") {
@@ -21,8 +32,8 @@ Array.prototype.customFilter = function (cb, thisArg) {
 };
 
 const createDebounceFunction = function (cb, time) {
-  if (typeof cb !== "function") {
-    throw new Error("Callback is not a function");
+  if (typeof cb !== "function" || !isValidTime(time)) {
+    throw new Error();
   }
   let isBusy = false;
   return function () {
