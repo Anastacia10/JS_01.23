eb308f133559a334139f9d695d20c44e7c193bdb
Anastacia10 <Anastacia10Job@gmail.com>
2023-02-15 20:06:14 +0100

    [5.2:15.02]: finished second task

--- index.js
+++ index.js
@@ -76,3 +76,109 @@ class Stack {
     return newArray;
   };
 }
+class LinkedList {
+  static fromIterable(iterable) {
+    if (typeof iterable[Symbol.iterator] !== "function") {
+      throw new Error("Not iterable");
+    } else {
+      const newList = new LinkedList();
+      for (let i = 0; i < iterable.length; i++) {
+        newList.append(iterable[i]);
+      }
+      return newList;
+    }
+  }
+  constructor() {
+    this._head = null;
+    this._tail = null;
+  }
+
+  get head() {
+    return this._head;
+  }
+
+  get tail() {
+    return this._tail;
+  }
+
+  set head(head) {
+    this._head = head;
+  }
+
+  set tail(tail) {
+    this._tail = tail;
+  }
+
+  append = (elem) => {
+    const newNode = new Node(elem);
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+  };
+
+  prepend = (elem) => {
+    const newNode = new Node(elem, this.head);
+    if (this.head) {
+      this.head.prev = newNode;
+    }
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+  };
+
+  find = (elem, currentNode = this.head) => {
+    if (currentNode === null) {
+      return null;
+    }
+    if (currentNode.value === elem) {
+      return currentNode;
+    }
+    return this.find(elem, currentNode.next);
+  };
+
+  toArray = (currentNode = this.head, array = [], index = 0) => {
+    if (currentNode === null) {
+      return array;
+    } else {
+      array[index] = currentNode.value;
+      return this.toArray(currentNode.next, array, index + 1);
+    }
+  };
+}
+class Node {
+  constructor(value, next = null, prev = null) {
+    this._value = value;
+    this._next = next;
+    this._prev = prev;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  get prev() {
+    return this._prev;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  set next(next) {
+    this._next = next;
+  }
+
+  set prev(prev) {
+    this._prev = prev;
+  }
+}
